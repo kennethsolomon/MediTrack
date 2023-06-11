62b959bea1aba4c5f45a61e84f60cb9502ac3024
kennethsolomon <mr.kennethsolomon@gmail.com>
2023-06-11 22:41:23 +0800

    testing

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    size: {
+        type: String,
+        default: "",
+    },
+
+    color: {
+        type: String,
+        default: "default",
+    },
+
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const sizeClass = computed(() => {
+    return {
+        default: "",
+        : "input-btn-primary",
+        neutral: "input-btn-neutral",
+        secondary: "input-btn-secondary",
+        accent: "input-btn-accent",
+        ghost: "input-btn-ghost",
+        link: "input-btn-link",
+    }[props.color];
+});
+
+const colorClass = computed(() => {
+    return {
+        default: "",
+        primary: "input-btn-primary",
+        neutral: "input-btn-neutral",
+        secondary: "input-btn-secondary",
+        accent: "input-btn-accent",
+        ghost: "input-btn-ghost",
+        link: "input-btn-link",
+    }[props.color];
+});
+</script>
+<template>
+    <button class="btn btn-active" :class="[colorClass]"><slot /></button>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,18 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
+
+import TextInput from "@/Components/FormInputs/TextInput.vue";
+
+const form_medicine = useForm({
+    name: "",
+});
+
+function submit(route_name, form) {
+    form_medicine.post(route(route_name), {
+        onSuccess: () => (form_medicine.name = ""),
+    });
+}
 </script>
 
 <template>
@@ -17,45 +29,29 @@ import { Head } from "@inertiajs/vue3";
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div
-                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
+                <form @submit.prevent="submit('medicine.store', form_medicine)">
+                    <TextInput
+                        id="medicine"
+                        name="name"
+                        type="text"
+                        v-model="form_medicine.name"
+                        placeholder="Medicine Name"
+                        color="primary"
+                        size="md"
+                        autofocus
+                    />
+                    <div v-if="form_medicine.errors.password">
+                        {{ form_medicine.errors.name }}
                     </div>
-                    <div class="alert alert-info">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            class="stroke-current shrink-0 w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                        </svg>
-                        <span>New software update available.</span>
-                    </div>
-                    <!-- Open the modal using ID.showModal() method -->
-                    <button class="btn" onclick="my_modal_1.showModal()">
-                        open modal
+
+                    <button
+                        type="submit"
+                        :disabled="form_medicine.processing"
+                        class="btn btn-active btn-primary"
+                    >
+                        Save
                     </button>
-                    <dialog id="my_modal_1" class="modal">
-                        <form method="dialog" class="modal-box">
-                            <h3 class="font-bold text-lg">Hello!</h3>
-                            <p class="py-4">
-                                Press ESC key or click the button below to close
-                            </p>
-                            <div class="modal-action">
-                                <!-- if there is a button in form, it will close the modal -->
-                                <button class="btn">Close</button>
-                            </div>
-                        </form>
-                    </dialog>
-                </div>
+                </form>
             </div>
         </div>
     </AuthenticatedLayout>
